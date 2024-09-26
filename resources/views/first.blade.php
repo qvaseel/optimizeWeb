9944ccb5ba5a2dd8fab0316ce9fb1933cc6e9109
JuliaMuk <mukhina_ur@mail.ru>
2024-09-26 12:03:27 +0500

    create layout main

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-<body>
+@extends('layouts.main')
+@section('content')
     <h1>Первая страница</h1>
     <p>{{ $sum }}</p>
     
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est soluta quas ad aspernatur cum distinctio alias voluptates nostrum harum magni, obcaecati odit ea sequi enim totam, amet perspiciatis aliquid dignissimos?</p>
-</body>
-</html>
+@endsection()
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        шапка
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        подвал
+    </footer>
+</body>
+</html>
